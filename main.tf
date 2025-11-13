81fa74ccad5df76eb17aa0f8f1b1637f54455aef
gus-shahatit <156473928+gus-shahatit@users.noreply.github.com>
2025-11-13 10:53:11 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,6 +33,18 @@ provider "aap" {
 
 provider "aws" {
   region = "us-east-2"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+variable "access_key" {
+  description = "AWS access key"
+  type        = string
+}
+
+variable "secret_key" {
+  description = "AWS access key secret"
+  type        = string
 }
 
 # Variable to store the public key for the EC2 instance
